c0195db0000e970901f8c5d4f17156d28cd8352e
JorgeGodina00 <92189696+JorgeGodina00@users.noreply.github.com>
2023-06-16 18:48:16 -0600

    Introduccion a Vistas
    Se hizo em manejo correcto de rutas, el guardado de datos

--- Practica5/app.py
+++ Practica5/app.py
@@ -1,11 +1,33 @@
+from flask import Flask, render_template, request #Importacion de librerias
 
-from flask import Flask
+#iniciar servidor Flask
+#configuracion de BD
+app= Flask(__name__)
+app.config['MYSQL_HOST']="localhost"
+app.config['MYSQL_USER']="root"
+app.config['MYSQL_PASSWWORD']=""
+app.config['MYSQL_DB']="dbflask"
 
-app = Flask(__name__)
-
+#Declaracion de la ruta
+#Ruta index
+#La ruta se compone de la ruta y su funcion
 @app.route('/')
 def index():
-    return "Hola Mundo Roman es Puto y se la mama al Teo"
+    return render_template('index.html')
 
-if __name__=='__main__':
-    app.run(port= 5000)
+@app.route('/guardar', methods=['POST'])
+def guardar():
+    if request.method == 'POST':
+        titulo= request.form['txtTitulo']
+        artista= request.form['txtArtista']
+        anio= request.form['txtAnio']
+    
+    return "La info del Album lelgo a su ruta Amigo :)"
+
+@app.route('/eliminar')
+def eliminar():
+    return
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
