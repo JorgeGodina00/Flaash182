daaf2e3b238c16a484d22e2a3fe8c2e5bba8121d
JorgeGodina00 <92189696+JorgeGodina00@users.noreply.github.com>
2023-07-04 18:53:51 -0600

    Practica 8 Editar
    Se realizo la creacion del template para editar registros, se comenzo el de editar

--- Practica5/app.py
+++ Practica5/app.py
@@ -38,10 +38,47 @@ def guardar():
     flash('Album Agregado Correctamente')    
     return redirect(url_for('index'))
 
-@app.route('/eliminar')
-def eliminar():
-    return "Se elimino"
+@app.route('/editar/<id>')
+def editar(id):  
+    curEditar= mysql.connection.cursor()
+    curEditar.execute('Select * from albums Where id= %s ', (id,))
+    consulId= curEditar.fetchone()
+    return render_template('editarAlbum.html',album= consulId)
 
+@app.route('/eliminar/<id>')
+def eliminar(id):
+    id= request.args.get('id')
+    curEliminar= mysql.connection.cursor()
+    curEliminar.execute('Select * from albums where id= %s', (id,))
+    consulId= curEliminar.fetchone()
+    return render_template('eliminarAlbum.html',album= consulId)
 
+@app.route('/actualizar/<id>', methods=['POST'])
+def actualizar(id):
+    if request.method == 'POST':
+        Vtitulo= request.form['txtTitulo']
+        Vartista= request.form['txtArtista']
+        Vanio= request.form['txtAnio']
+
+        CS= mysql.connection.cursor()
+        CS.execute("UPDATE albums SET titulo= %s, artista= %s, anio= %s WHERE id= %s", (Vtitulo, Vartista, Vanio, id))
+        mysql.connection.commit()
+        
+    flash("Album Actualizado Correctamente")
+    return redirect(url_for('index'))
+
+@app.route('/eliminar2/<id>', methods=['POST'])
+def eliminar2(id):
+    if request.method == 'POST':
+        Vtitulo= request.form['txtTitulo']
+        Vartista= request.form['txtArtista']
+        Vanio= request.form['txtAnio']
+
+        CS= mysql.connection.cursor()
+        CS.execute("delete from albums WHERE id= %s", (Vtitulo, Vartista, Vanio, id))
+        mysql.connection.commit()
+        
+    flash("Album Actualizado Correctamente")
+    return redirect(url_for('index'))
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
