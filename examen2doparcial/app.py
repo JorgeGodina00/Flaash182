b963229db300fab2277e88a2c3fa8858c80f0acb
JorgeGodina00 <92189696+JorgeGodina00@users.noreply.github.com>
2023-07-14 18:51:07 -0600

    examen 2do parcial
    2do commit

--- examen2doparcial/app.py
+++ examen2doparcial/app.py
@@ -32,24 +32,16 @@ def guardar():
         CS = mysql.connection.cursor() 
         CS.execute("INSERT INTO tbflores (nombre, cantidad, precio) VALUES (%s, %s, %s)", (Vnombre, Vcantidad, Vprecio))
         mysql.connection.commit()
-    flash('Album Agregado Correctamente')    
+    flash('Registro Agregado Correctamente')    
     return redirect(url_for('index'))
 
 
-@app.route('/eliminar/<id>')
-def eliminar(id):
-    id= request.args.get('id')
-    curEliminar= mysql.connection.cursor()
-    curEliminar.execute('Select * from tbflores where id= %s', (id,))
-    consulId= curEliminar.fetchone()
-    return render_template('eliminarflores.html',flor= consulId)
-
-
-@app.route('/eliminar2/<id>', methods=['POST'])
-def eliminar2(id):
-
-    if request.method == 'POST':
-        Vnombre= request.form['txtNombre']
+@app.route('/eliminar/<id>', method='POST')
+def delete(id):
+    
+    if request.method == "POST":
+        if request.form.get("confirm") == "yes":
+                    Vnombre= request.form['txtNombre']
         Vcantidad= request.form['txtCantidad']
         Vprecio= request.form['txtPrecio']
 
@@ -57,7 +49,16 @@ def eliminar2(id):
         CS.execute("delete from tbflores WHERE id= %s", (Vnombre, Vcantidad, Vprecio, id))
         mysql.connection.commit()
         
-    flash("Registro Eliminado Correctamente")
-    return redirect(url_for('index'))
+        flash("Registro Eliminado Correctamente")
+        return redirect(url_for('index'))            
+    
+    else:
+        
+        return render_template("index.html")
+
+
+
+
+
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
